9bed5773668d004f6ccee054795d235105c2d1e9
alexshangin96-ux <alexey.shangin96@gmail.com>
2025-10-26 09:31:55 +0700

    Added generateReferralCode function

--- server.js
+++ server.js
@@ -337,6 +337,16 @@ function extractUserData(initData) {
     }
 }
 
+// Функция генерации реферального кода
+function generateReferralCode() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+    for (let i = 0; i < 8; i++) {
+        code += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return code;
+}
+
 // Middleware для проверки авторизации
 function requireAuth(req, res, next) {
     const initData = req.headers['x-telegram-init-data'];
